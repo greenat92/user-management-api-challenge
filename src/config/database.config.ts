7e553465f367bc14654db7d404127fbcba403580
LBenzahia <lakhdar.benzahia@gmail.com>
2024-07-23 22:05:46 +0200

    fix database config

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,10 +1,9 @@
-// src/config/database.config.ts
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'sqlite',
   database: 'db.sqlite',
-  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  entities: [__dirname + '/../../**/*.entity.{js,ts}'],
   synchronize: process.env.APP_ENV !== 'production',
   logging: process.env.APP_ENV !== 'production',
 };
